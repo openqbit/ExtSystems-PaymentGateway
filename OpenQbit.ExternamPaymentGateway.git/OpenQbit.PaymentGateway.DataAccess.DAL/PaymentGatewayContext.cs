397f944d9d2fa1834ef164c34c3264c35569e610
Sarasi Aluthge <sarasialuthge14@gmail.com>
2017-05-22 15:54:39 +0530

    DAL created

---
+++ OpenQbit.ExternamPaymentGateway.git/OpenQbit.PaymentGateway.DataAccess.DAL/PaymentGatewayContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using OpenQbit.PaymentGateway.Common.Models;
+
+namespace OpenQbit.PaymentGateway.DataAccess.DAL
+{
+    public class PaymentGatewayContext : DbContext 
+    {
+        public PaymentGatewayContext() : base("OpenQbit.PaymentGatewayDB") {}
+        public DbSet<Bank> Bank { get; set; }
+        public DbSet<Merchant> Merchant { get; set; }
+        public DbSet<Request> Request { get; set; }
+        public DbSet<Responce> Responce { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
